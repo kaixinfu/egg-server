ad13c398c1d0a175b35ce1e46ca0f35b481b2619
liu kaixin <1360517661@qq.com>
2020-05-17 12:47:22 +0800

    chore(home.js): test

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,10 +1,12 @@
 'use strict'
 
-const Controller = require('egg').Controller;
+const Controller = require('egg').Controller
 
 class HomeController extends Controller {
   async index() {
     const { ctx } = this
+    console.log('aaaa')
+    
     ctx.body = 'hi, egg'
   }
 }
